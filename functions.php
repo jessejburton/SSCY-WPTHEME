8f25619262d63365379e33b79b4500cdb1025c54
Jesse James Burton <jesse@burtonmediainc.com>
2017-10-30 11:52:22 -0700

    Added social media links and widget shortcode functionality.

--- functions.php
+++ functions.php
@@ -57,6 +57,9 @@ function sscy_theme_setup(){
     'before_title' => '<h6 class="widget-title">',
     'after_title' => '</h6>',
   ) );
+
+  // Enable shortcodes in html widgets
+  add_filter('widget_text','do_shortcode');
 }
 add_action( 'after_setup_theme', 'sscy_theme_setup' );
 
@@ -133,6 +136,10 @@ function sscy_register_js() {
 add_action( 'init', 'sscy_register_js' );
 // CSS
 function sscy_register_css() {
+    // Font Awesome
+    wp_register_style( 'font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' );
+    wp_enqueue_style( 'font_awesome' ); 
+
     wp_register_style( 'google_fonts', 'https://fonts.googleapis.com/css?family=Crimson+Text:400i|Roboto:300,400' );
     wp_enqueue_style( 'google_fonts' );   
     
@@ -143,6 +150,13 @@ function sscy_register_css() {
     wp_enqueue_style( 'sscy_secondary_styles' ); 
 }
 add_action( 'wp_enqueue_scripts', 'sscy_register_css' );
+// CSS
+function sscy_register_admin_css() {
+    // Font Awesome
+    wp_register_style( 'font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' );
+    wp_enqueue_style( 'font_awesome' ); 
+}
+add_action( 'admin_enqueue_scripts', 'sscy_register_admin_css' );
 
 /*
 *	Function to display child pages
@@ -162,3 +176,120 @@ function sscy_list_child_pages() {
 	 
 	return $string;
 }
+
+
+ // ------------------------------------------------------------------
+ // Add all your sections, fields and settings during admin_init
+ // ------------------------------------------------------------------
+ //
+ 
+ function sscy_setting_api_init() {
+  // Add the section to reading settings so we can add our
+  // fields to it
+  add_settings_section(
+    'sscy_social_media_setting_section',
+    'Social Media Links',
+    'sscy_social_media_setting_section_callback_function',
+    'general'
+  );
+  
+  // Add the field with the names and function to use for our new
+  // settings, put it in our new section
+  //FACEBOOK
+  add_settings_field(
+    'facebook_setting_name',
+    'Facebook URL',
+    'sscy_facebook_setting_callback_function',
+    'general',
+    'sscy_social_media_setting_section'
+  );
+  register_setting( 'general', 'facebook_setting_name' );
+  // TWITTER
+  add_settings_field(
+    'twitter_setting_name',
+    'Twitter URL',
+    'sscy_twitter_setting_callback_function',
+    'general',
+    'sscy_social_media_setting_section'
+  );
+  register_setting( 'general', 'twitter_setting_name' );  
+  // YOUTUBE
+  add_settings_field(
+    'youtube_setting_name',
+    'YouTube URL',
+    'sscy_youtube_setting_callback_function',
+    'general',
+    'sscy_social_media_setting_section'
+  );
+  register_setting( 'general', 'youtube_setting_name' );
+  // INSTAGRAM
+  add_settings_field(
+    'instagram_setting_name',
+    'Instagram URL',
+    'sscy_instagram_setting_callback_function',
+    'general',
+    'sscy_social_media_setting_section'
+  );
+  register_setting( 'general', 'instagram_setting_name' );  
+
+ } // sscy_setting_api_init()
+ 
+ add_action( 'admin_init', 'sscy_setting_api_init' );
+ 
+  
+ // ------------------------------------------------------------------
+ // Settings section callback function
+ // ------------------------------------------------------------------
+ //
+ // This function is needed if we added a new section. This function 
+ // will be run at the start of our section
+ //
+ 
+ function sscy_social_media_setting_section_callback_function() {
+  echo '<p>Enter your social media links here and then use the shortcode [sscy_socialmedia] to display them on the site.</p>';
+ }
+ 
+ // ------------------------------------------------------------------
+ // Callback function for our example setting
+ // ------------------------------------------------------------------
+ //
+ // creates a checkbox true/false option. Other types are surely possible
+ //
+ 
+ function sscy_facebook_setting_callback_function() {
+  echo '<input name="facebook_setting_name" id="facebook_setting_name" type="text" class="regular-text" value="' . get_option( 'facebook_setting_name' ) . '" /> <i class="fa fa-facebook-square"></i>';
+ }
+  function sscy_twitter_setting_callback_function() {
+  echo '<input name="twitter_setting_name" id="twitter_setting_name" type="text" class="regular-text" value="' . get_option( 'twitter_setting_name' ) . '" /> <i class="fa fa-twitter-square"></i>';
+ }
+  function sscy_youtube_setting_callback_function() {
+  echo '<input name="youtube_setting_name" id="youtube_setting_name" type="text" class="regular-text" value="' . get_option( 'youtube_setting_name' ) . '" /> <i class="fa fa-youtube-square"></i>';
+ }
+  function sscy_instagram_setting_callback_function() {
+  echo '<input name="instagram_setting_name" id="instagram_setting_name" type="text" class="regular-text" value="' . get_option( 'instagram_setting_name' ) . '" /> <i class="fa fa-instagram"></i>';
+ }
+
+
+// Short code for Social Media
+function sscy_socialmedia_shortcode(){
+  ob_start();
+  ?> 
+    <ul class="socialmedia_links">
+      <?php if( get_option( 'facebook_setting_name' ) != '' ) { ?>
+        <li><a href="<?php echo get_option( 'facebook_setting_name' ) ?>" class="sscy-socialmedia-link" title="Find us on Facebook" target="_blank"><i class="fa fa-facebook-square"></i></a></li>
+      <?php } ?>
+      <?php if( get_option( 'twitter_setting_name' ) != '' ) { ?>
+        <li><a href="<?php echo get_option( 'twitter_setting_name' ) ?>" class="sscy-socialmedia-link" title="Find us on Twitter" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
+      <?php } ?>
+      <?php if( get_option( 'youtube_setting_name' ) != '' ) { ?>
+        <li><a href="<?php echo get_option( 'youtube_setting_name' ) ?>" class="sscy-socialmedia-link" title="Find us on YoutTube" target="_blank"><i class="fa fa-youtube-square"></i></a></li>
+      <?php } ?>
+      <?php if( get_option( 'instagram_setting_name' ) != '' ) { ?>
+        <li><a href="<?php echo get_option( 'instagram_setting_name' ) ?>" class="sscy-socialmedia-link" title="Find us on Instagram" target="_blank"><i class="fa fa-instagram"></i></a></li>
+      <?php } ?>
+    </ul>
+  <?php
+  return ob_get_clean();
+}
+add_shortcode( 'sscy_socialmedia', 'sscy_socialmedia_shortcode' );
+
