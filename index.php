6867431b72e6a25f4a08454ab581eaee7b19762e
Jesse James Burton <jesse@burtonmediainc.com>
2017-11-02 16:11:08 -0700

    Changeed the no sidebar to be the default template.

--- index.php
+++ index.php
@@ -1,29 +1,17 @@
 <?php
 get_header();
 
-?><main>
-	
-	<section class="has-sidebar blog"><div class="loop"><?php
-		if (have_posts()) :
-		   while (have_posts()) :
-		      the_post();
-		      	 ?>
-		      	 	<article><header><h1><?php the_title(); ?></h1></header></article>
-		      	 	<article><?php the_content(); ?></article>
-		      	 <?php
-		   endwhile;
-		endif;
-		?><aside><h4>Latests Posts</h4><ul><?php
-			$recent_posts = wp_get_recent_posts();
-			foreach( $recent_posts as $recent ){
-				echo '<li><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"].'</a></li> ';
-			}
-			wp_reset_query();
-		?></ul></aside><?php
-	?></div></section><?php
+?><main><div class="loop"><?php
 
+	if (have_posts()) :
+	   while (have_posts()) :
+	      the_post();
+			set_query_var( 'current_post', $post );
+            echo get_template_part( 'templates/panel' );
+	   endwhile;
+	endif;
 
-?></main><?php
+?></div></main><?php
 
-	get_footer(); 
+get_footer(); 
 ?>
