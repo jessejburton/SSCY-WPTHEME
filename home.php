4b77c4ebb854c7275f93d79d63c20e7c81acab66
Jesse James Burton <jesse@burtonmediainc.com>
2017-11-02 17:44:05 -0700

    Adjustments to blog output

--- home.php
+++ home.php
@@ -8,18 +8,18 @@ get_header();
 		   while (have_posts()) :
 		      the_post();
 		      	 ?>
-		      	 	<article>
-		      	 		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="post-thumbnail">
-							<?php the_post_thumbnail( $size = 'post-thumbnail', $attr = '' ); ?>
-						</a>
-			      	 	<header>
-							<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-						</header>
-			      	 	<?php the_excerpt(); ?>
-			      	 	<footer>
-							<p>POSTED ON <?php the_time( 'F jS, Y' ); ?> by <?php the_author_posts_link(); ?>.</p>
-						</footer>
-			      	</article>
+				<article>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="post-thumbnail">
+						<?php the_post_thumbnail('',['class' => 'thumbnail']); ?>
+					</a>
+					<header>
+						<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+					</header>
+					<?php the_excerpt(); ?>
+					<footer>
+						<p>Posted on <?php the_time( 'F jS, Y' ); ?> by <?php the_author_posts_link(); ?>.</p>
+					</footer>
+				</article>
 		      	 <?php
 		   endwhile;
 		endif;
