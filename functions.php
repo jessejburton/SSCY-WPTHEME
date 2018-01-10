8eff0b2d6028e98b44917f6aabac09bc7459e939
Jesse James Burton <jesse@burtonmediainc.com>
2018-01-10 12:32:52 -0800

    Same as last commit. Found another glitch
    Signed-off-by: Jesse James Burton <jesse@burtonmediainc.com>

--- functions.php
+++ functions.php
@@ -164,7 +164,7 @@ function sscy_list_child_pages() {
 	global $post; 
 	 
 	if ( is_page() && $post->post_parent )	 
-	    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post . '&echo=0' );
+	    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
 	else
 	    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
 
