1aa97b9cc81af00a1ff47facb92123b6f0fcbb91
Jesse James Burton <jesse@burtonmediainc.com>
2018-02-28 21:08:12 -0600

    Added donation button

--- footer.php
+++ footer.php
@@ -566,6 +566,16 @@
 				</svg>
 		</a>
 	</div>
+	
+	<div style="position: absolute; right: 20px; bottom: 20px;">
+		<strong>Support Us</strong>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" id="pp_donate">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="DCENY95YPBQHS">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+	</div>
 </footer>
 
 
