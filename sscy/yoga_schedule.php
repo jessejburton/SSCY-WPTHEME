1f5675c4a62e924fc10a15c0c191dd8843344778
Jesse James Burton <jesse@burtonmediainc.com>
2018-05-11 11:55:46 -0700

    Added Facebook Pixel Code

--- sscy/yoga_schedule.php
+++ sscy/yoga_schedule.php
@@ -3,9 +3,12 @@
     // Set up the database connection
     require_once('mysqli_connect.php');
 
+    // Include the utility functions
+    require_once('utilities.php');
+
     $qry_classes = $sscy_database->get_results( "
         SELECT c.*, cs.*, t.*, a.* 
-        FROM class_schedule_tbl cs 
+        FROM class_weekly_schedule_tbl cs 
         LEFT JOIN class_tbl c ON c.class_id = cs.class_id
         LEFT JOIN teacher_tbl t ON c.teacher_id = t.teacher_id 
         LEFT JOIN account_tbl a ON t.account_id = a.account_id    
@@ -29,20 +32,30 @@
     $current_date = date(strtotime('-'.$today.' days'));
     
     ?>
+
+        <a href="http://localhost/SSCY/print/yoga_schedule.php" target="_blank"><i class="fas fa-print"></i> print schedule</a>
     
         <table class="table table--class">
-            <tr>
-                <th>Class</th>
-                <th>Time</th>
-                <th>Teacher</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th colspan="3" class="clearfix">
+                        <a href="#" class="table__navigation-link table__navigation-link--prev">
+                            <i class="far fa-arrow-alt-circle-left"></i> previous week
+                        </a>
+                        <a  class="
+                    table__navigation-link table__navigation-link--next" href="#">
+                            next week <i class="far fa-arrow-alt-circle-right"></i>
+                        </a>
+                    </th>
+                </tr>
+            </thead>
             <tbody>
 
                 <?php for ($i = 0; $i <= 6; $i++){ ?>   
                     
                     <!-- Display the date heading -->
                     <tr class="class-date">
-                        <td colspan="3"><?php echo displayDate($current_date); ?></td>
+                        <td colspan="3"><strong style="text-transform: uppercase;"><?php echo date('l', $current_date); ?></strong> <em style="font-size: .8em;"><?php echo date('F jS, Y', $current_date) ?></em></td>
                     </tr>
 
                     <?php if (sizeof($arr_classes[$i]) == 0) { ?>
@@ -56,15 +69,15 @@
                     <?php foreach ( $arr_classes[$i] as $class) { 
                         
                         // Set up the times
-                        $start_time = date_create('2000-01-01 ' . $arr_classes[0][0]->start_time)->format('g:iA'); 
-                        $end_time = date_create('2000-01-01 ' . $arr_classes[0][0]->end_time)->format('g:iA'); 
+                        $start_time = date_create('2000-01-01 ' . $class->start_time)->format('g:iA'); 
+                        $end_time = date_create('2000-01-01 ' . $class->end_time)->format('g:iA'); 
 
                     ?> 
 
                         <tr class="class">
+                            <td><a class="class__description_link" href="javascript:void(0);" title="<?php echo $class->name; ?>"><?php echo excerpt($class->name, 30); ?></a></td>        
+                            <td><a class="class__teacher_link" href="javascript:void(0);"><?php echo $class->name_first . ' ' . $class->name_last; ?></a></td>
                             <td><?php echo $start_time . ' - ' . $end_time; ?></td>
-                            <td><a class="class__description_link" href="#"><?php echo $class->name; ?></a></td>
-                            <td><a class="class__teacher_link" href="#"><?php echo $class->name_first . ' ' . $class->name_last; ?></a></td>
                         </tr>
                         <tr class="class__details class__details--description">
                             <td colspan="4">
